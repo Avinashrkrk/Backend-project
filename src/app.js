af780ee7dc7d6d5e272bfd6cf0bc6db5f5647ee4
Avinash Pathak <avipathak732@gmail.com>
2024-12-05 12:52:48 +0530

    All setup configured

--- src/app.js
+++ src/app.js
@@ -17,5 +17,13 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static('public'))
 app.use(cookieParser())
 
+//routes
+
+import userRouter from './routes/user.routes.js'
+
+
+
+// routes declaraton
+app.use("/api/v1/users", userRouter)
 
 export {app} 
